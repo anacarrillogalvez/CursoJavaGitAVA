17c327f7eabe0168a00da4cdb89742d0082bffc5
AaronVelasco93 <aaronvelasco1993@outlook.com>
2022-01-19 13:20:27 -0600

    Se agrega el nombre al mensaje de salida

--- holamundo.java
+++ holamundo.java
@@ -1,6 +1,7 @@
 public class holamundo{
         public static void main(String [] args){
                 System.out.println("Hola mundo desde terminal lrsd");
+                  System.out.println("Hola mundo desde terminal Paulina");
 
         }
 }
